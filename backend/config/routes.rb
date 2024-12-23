fc4eabcaba1f55ec2ea7a2770e7fe0962a2d9179
Kuroneko <ken.aster8@gmail.com>
2024-12-23 21:38:13 +0900

    ルーティングの修正とdevise_token_authの設定修正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     get "health_check", to: "health_check#index"
+    mount_devise_token_auth_for "User", at: "auth"
   end
 end
