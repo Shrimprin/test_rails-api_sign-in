d1e21712757b2ce147c7ad78a11fe21324355659
Kuroneko <ken.aster8@gmail.com>
2024-12-23 22:06:55 +0900

    タイポ修正

--- backend/app/controllers/api/base_controller.rb
+++ backend/app/controllers/api/base_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Api::V1::BaseController < ApplicationController
+class Api::BaseController < ApplicationController
   alias current_user current_api_user
   alias authenticate_user! authenticate_api_user!
   alias user_signed_in? api_user_signed_in?
