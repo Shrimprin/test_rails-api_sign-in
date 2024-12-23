5bb5b73ea87b3734a4f98a362deef301e95909d8
Kuroneko <ken.aster8@gmail.com>
2024-12-23 22:17:25 +0900

    現在のサインインユーザーの情報を取得するapi作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     get "health_check", to: "health_check#index"
     mount_devise_token_auth_for "User", at: "auth"
+    namespace :current do
+      resource :user, only: [:show]
+    end
   end
 end
