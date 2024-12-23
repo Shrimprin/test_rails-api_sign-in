46e65a7f67b15de513515e5f35d1f174550dc4f7
Kuroneko <ken.aster8@gmail.com>
2024-12-23 22:25:18 +0900

    serializerでレスポンスJSONを成形するようにした

--- backend/app/controllers/api/current/users_controller.rb
+++ backend/app/controllers/api/current/users_controller.rb
@@ -4,6 +4,6 @@ class Api::Current::UsersController < Api::BaseController
   before_action :authenticate_user!
 
   def show
-    render json: current_user
+    render json: current_user, serializer: CurrentUserSerializer
   end
 end
